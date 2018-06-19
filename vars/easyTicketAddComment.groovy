44fd7356b6befbbfc85dc28b43d48c6b37e51087
Marco Westermann <mw@intersales.de>
2018-06-19 13:04:07 +0200

    other way to build xml

--- vars/easyTicketAddComment.groovy
+++ vars/easyTicketAddComment.groovy
@@ -10,15 +10,10 @@ import groovy.xml.MarkupBuilder
  */
 def call(Map data) {
     
-    def sw = new StringWriter()
-    def mb = new MarkupBuilder(sw)
-    mb.issue {
-        notes (data.comment)
-    }
+    def xml = """<issue><notes>${data.comment}</notes></issue>"""
     
-    println("using the following put request data:\n${sw.toString()}")
-    
-    new Utils().put("${data.easyredmineBaseUrl}/issues/${data.ticketNo.toString()}.xml", sw.toString(), data.authKey)
+    new Utils().put("${data.easyredmineBaseUrl}/issues/${data.ticketNo.toString()}.xml",
+            xml, data.authKey)
 }
 
 return this
