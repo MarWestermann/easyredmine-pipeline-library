8156446248c0048a4e3a41680dc8817a00a759a3
Marco Westermann <mw@intersales.de>
2018-06-19 14:13:26 +0200

    fixed bug

--- src/de/intersales/jenkins/easyredmine/EasyredmineHelper.groovy
+++ src/de/intersales/jenkins/easyredmine/EasyredmineHelper.groovy
@@ -1,7 +1,10 @@
 package de.intersales.jenkins.easyredmine
 
+import com.cloudbees.groovy.cps.NonCPS
+
 class EasyredmineHelper {
     
+    @NonCPS
     String mapStatusNameToId(Map data) {
         def xml = new Utils().get("${data.easyredmineBaseUrl}/issue_statuses.xml", data.authKey)
         def xmlObject = new XmlSlurper().parseText(xml)
